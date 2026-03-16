020282a9e640cdb021a0994869e54d996b186672
James Kind <7375739+kindjames@users.noreply.github.com>
2026-03-16 16:40:16 +0000

    Fix sha256 for public repo tarball

--- Formula/exifimport.rb
+++ Formula/exifimport.rb
@@ -2,7 +2,7 @@ class Exifimport < Formula
   desc "Ingest media from a memory card into an opinionated date-based directory structure"
   homepage "https://github.com/kindjames/exifimport"
   url "https://github.com/kindjames/exifimport/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "eab2a609b189359e73e43b1194e9b5496c75d9b2f9e7d2a4c8fca2bce75c67fd"
   license "ISC"
 
   depends_on "node"
