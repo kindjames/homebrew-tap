51e9616996416b406a665b4e6a4a3d11105514cf
James Kind <7375739+kindjames@users.noreply.github.com>
2026-03-16 16:51:52 +0000

    Bump exifimport to v0.0.2

--- Formula/exifimport.rb
+++ Formula/exifimport.rb
@@ -1,8 +1,8 @@
 class Exifimport < Formula
   desc "Ingest media from a memory card into an opinionated date-based directory structure"
   homepage "https://github.com/kindjames/exifimport"
-  url "https://github.com/kindjames/exifimport/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "eab2a609b189359e73e43b1194e9b5496c75d9b2f9e7d2a4c8fca2bce75c67fd"
+  url "https://github.com/kindjames/exifimport/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "4d48f91ec0c82fe6a93fe00bcc897aea1c134233d35febb6140306629a2963bc"
   license "ISC"
 
   depends_on "node"
